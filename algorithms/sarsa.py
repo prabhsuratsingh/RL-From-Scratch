f0ca5c359285fa9bedf70cbbc236de33bd40a917
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-27 20:27:11 +0530

    implement SARSA algorithm

---
+++ algorithms/sarsa.py
@@ -0,0 +1,68 @@
+from collections import namedtuple
+
+from envs.gridworld import GridWorldEnv
+from agents.agent import Agent
+from utils.plots import plot_learning_history
+
+Transition = namedtuple(
+    'Transition',
+    ('state', 'action', 'reward', 'next_state', 'next_action', 'terminated', 'truncated')
+)
+
+def run_sarsa(agent, env, num_episodes=50, max_steps=500):
+    history = []
+
+    for episode in range(num_episodes):
+        state, _ = env.reset()
+        env.render()
+
+        final_reward, n_moves = 0.0, 0
+
+        for step in range(max_steps):
+            action = agent.choose_action(state)
+
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            next_action = agent.choose_action(next_state)
+
+            agent.learn(
+                Transition(
+                    state,
+                    action,
+                    reward,
+                    next_state,
+                    next_action,
+                    terminated,
+                    truncated
+                )
+            )
+
+            env.render()
+
+            state, action = next_state, next_action
+            n_moves += 1
+            final_reward = reward
+
+            if terminated:
+                break
+
+        else:
+            truncated = True
+
+        history.append((n_moves, final_reward))
+        print(
+            f"Episode {episode}: "
+            f"Reward {final_reward:.2f} "
+            f"#Moves {n_moves}"
+        )
+
+    return history
+
+
+if __name__ == "__main__":
+    env = GridWorldEnv(num_rows=5, num_cols=6, render_mode="human")
+    agent = Agent(env, algorithm="sarsa")
+
+    history = run_sarsa(agent, env,)
+
+    env.close()
+    plot_learning_history(history, "gridworld", "sarsa")
--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 from agents.agent import Agent
 from algorithms.qlearning import run_qlearning
+from algorithms.sarsa import run_sarsa
 from envs.cliff_walking import CliffWalkingEnv
 from envs.gridworld import GridWorldEnv
 from utils.plots import plot_learning_history
 
 
-env = CliffWalkingEnv(render_mode="human")
-agent = Agent(env, algorithm="q_learning")
+# env = CliffWalkingEnv(render_mode="human")
+# agent = Agent(env, algorithm="q_learning")
 
-history = run_qlearning(agent, env)
-
-env.close()
-plot_learning_history(history, "cliffwalking", "q-learning")
-
-# env = GridWorldEnv(num_rows=5, num_cols=6, render_mode="human")
-# agent = Agent(env)
-
-# history = run_qlearning(agent, env,)
+# history = run_qlearning(agent, env)
 
 # env.close()
-# plot_learning_history(history, "gridworld", "q-learning")
+# plot_learning_history(history, "cliffwalking", "q-learning")
+
+env = GridWorldEnv(num_rows=5, num_cols=6, render_mode="human")
+agent = Agent(env, algorithm="sarsa")
+
+history = run_sarsa(agent, env,)
+
+env.close()
+plot_learning_history(history, "gridworld", "sarsa")
