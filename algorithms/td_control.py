2c43aa671f09f3f3bee8563cb215478ded8f83c4
prabhsuratsingh <prabhsurat17@gmail.com>
2026-02-04 18:08:00 +0530

    implement unified TD algorithm runner

---
+++ algorithms/td_control.py
@@ -0,0 +1,63 @@
+from agents.double_td_agent import DoubleTDAgent
+from envs.cliff_walking import CliffWalkingEnv
+from envs.gridworld import GridWorldEnv
+from agents.agent import Agent
+from utils.plots import plot_learning_history
+
+
+def run_td_control(agent, env, num_episodes=50, max_steps=500):
+    history = []
+
+    for episode in range(num_episodes):
+        state, _ = env.reset()
+        env.render()
+
+        final_reward, n_moves = 0.0, 0
+
+        for step in range(max_steps):
+            action = agent.choose_action(state)
+
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            transition = (
+                state,
+                action,
+                reward,
+                next_state,
+                terminated,
+                truncated
+            )
+
+            agent.learn(transition)
+
+            env.render()
+
+            state = next_state 
+            n_moves += 1
+            final_reward = reward
+
+            if terminated:
+                break
+
+        else:
+            truncated = True
+
+        history.append((n_moves, final_reward))
+        print(
+            f"Episode {episode}: "
+            f"Reward {final_reward:.2f} "
+            f"#Moves {n_moves}"
+        )
+
+    return history
+
+
+if __name__ == "__main__":
+    # env = GridWorldEnv(num_rows=5, num_cols=6, render_mode="human")
+    env = CliffWalkingEnv(num_rows=5, num_cols=6, render_mode="human")
+    agent = DoubleTDAgent(env, algorithm="double_q_learning")
+
+    history = run_td_control(agent, env,)
+
+    env.close()
+    plot_learning_history(history, "cliff_walking", "double_q_learning", save_dir="experiments/td")
